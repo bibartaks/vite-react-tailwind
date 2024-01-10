fb07ba1f73f4881c71ae12527a4b2b16cfb6c6ab
bibartaks <bibartaks@gmail.com>
2024-01-10 21:15:41 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-center mt-20">
+        Welcome to Vite with TailwindCSS and React with TypeScript
+      </h1>
+    </>
+  )
+}
+
+export default App
